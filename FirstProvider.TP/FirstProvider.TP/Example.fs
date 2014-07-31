3921ef2158b6399a685fe90247866e331bd6a462
victor-dumitrescu <s1107546@sms.ed.ac.uk>
2014-07-31 19:33:14 +0100

    Trying to add static parameter through combinator

--- FirstProvider.TP/FirstProvider.TP/Example.fs
+++ FirstProvider.TP/FirstProvider.TP/Example.fs
@@ -2,18 +2,26 @@
 
 open FSharp.ProvidedTypes.GeneralCombinators
 open FSharp.ProvidedTypes.CloneCombinator
+open FSharp.ProvidedTypes.AddStaticCombinator
 open Microsoft.FSharp.Core.CompilerServices
 
-let Example1 = 
-    
-    let SimpleProvider =
-//        let FSharpDataAssembly = typeof<MyTypeProvider>.Assembly
-        new FirstProvider.TP.MyTypeProvider()
+//let Example1 = 
+//    let SimpleProvider =
+//        new FirstProvider.TP.MyTypeProvider()
+//    Clone("FirstTypeProvider.Something", "MySpace", SimpleProvider)
 
-    Clone("FirstTypeProvider.Something", "MySpace", SimpleProvider)
+
+let ExampleAddStatic = 
+    let SimpleProvider =
+        Clone("FirstTypeProvider.Something", "MySpace", new FirstProvider.TP.MyTypeProvider())
+    AddStaticParameter("AnotherOne", typeof<string>, "defaultv", SimpleProvider)
+
+//[<TypeProvider>]
+//type ExampleProvider() = inherit TypeProviderExpression(Example1)
 
 [<TypeProvider>]
-type ExampleProvider() = inherit TypeProviderExpression(Example1)
+type AddStaticExample() = inherit TypeProviderExpression(ExampleAddStatic)
 
 [<assembly:TypeProviderAssembly>]
 do()
+    
--- FirstProvider.TP/tests/Tests/Tests.fs
+++ FirstProvider.TP/tests/Tests/Tests.fs
@@ -3,6 +3,7 @@
 // #r @"C:\GitHub\victor-dumitrescu\simple-providers\FirstProvider.TP\FirstProvider.TP\bin\Debug\FirstProvider.TP.dll"
 type Mine = MySpace.NewType<"stuff">
 
+
 printfn "%A" Mine.Hello
 
 let thing = new Mine(42)
